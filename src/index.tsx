3dd476e80115b121fc30d23214b23c89c8ab78d7
Thi Nguyen <anhthi.ieig@gmail.com>
2023-02-21 08:11:02 +0700

    Basic setup

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import * as ReactDOMClient from "react-dom/client";
+
+import App from "./App";
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = ReactDOMClient.createRoot(document.getElementById("root")!);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
